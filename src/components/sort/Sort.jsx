a4ba36f2503d30e8007dc87b88f3c6d6e57b44c8
Pavel Zolotin <zolotinpavel@gmail.com>
2022-08-15 19:33:14 +0500

    add pop-up display state

--- src/components/sort/Sort.jsx
+++ src/components/sort/Sort.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 function Sort() {
+    const [openSort, setOpenSort] = useState(false);
+
     return (
         <div className="sort">
             <div className="sort__label">
@@ -15,15 +19,17 @@ function Sort() {
                     />
                 </svg>
                 <b>Сортировка по:</b>
-                <span>популярности</span>
+                <span onClick={() => setOpenSort(!openSort)}>популярности</span>
             </div>
-            <div className="sort__popup">
-                <ul>
-                    <li className="active">популярности</li>
-                    <li>цене</li>
-                    <li>алфавиту</li>
-                </ul>
-            </div>
+            {openSort && (
+                <div className="sort__popup">
+                    <ul>
+                        <li className="active">популярности</li>
+                        <li>цене</li>
+                        <li>алфавиту</li>
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
