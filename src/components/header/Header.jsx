ed09cddf8cf591e5759a2dd733cc41cc7a29e870
Pavel Zolotin <zolotinpavel@gmail.com>
2022-08-16 14:40:35 +0500

    add click on the logo takes to the home page without reloading

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,20 @@
+import { Link } from 'react-router-dom';
+
 import Logo from '../../assets/img/pizza-logo.svg';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="container">
-                <div className="header__logo">
-                    <img width="38" src={Logo} alt="Pizza logo"/>
-                    <div>
-                        <h1>React Pizza</h1>
-                        <p>самая вкусная пицца во вселенной</p>
+                <Link to="/">
+                    <div className="header__logo">
+                        <img width="38" src={Logo} alt="Pizza logo"/>
+                        <div>
+                            <h1>React Pizza</h1>
+                            <p>самая вкусная пицца во вселенной</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
                 <div className="header__cart">
                     <a href="/cart.html" className="button button--cart">
                         <span>520 ₽</span>
