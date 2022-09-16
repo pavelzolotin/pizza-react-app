c2704bdb00e376cf7bf7813b476942ff06e3f3bd
Pavel Zolotin <zolotinpavel@gmail.com>
2022-09-16 17:49:44 +0500

    add changes in app styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,14 +10,14 @@ function Header() {
                     <div className="header__logo">
                         <img width="38" src={Logo} alt="Pizza logo"/>
                         <div>
-                            <h1>React Pizza</h1>
-                            <p>самая вкусная пицца во вселенной</p>
+                            <h1>Pizza Joe</h1>
+                            <p>самая вкусная пицца</p>
                         </div>
                     </div>
                 </Link>
                 <div className="header__cart">
                     <Link to="/cart" className="button button--cart">
-                        <span>520 ₽</span>
+                        <span>550 ₽</span>
                         <div className="button__delimiter"></div>
                         <svg
                             width="18"
@@ -47,7 +47,7 @@ function Header() {
                                 strokeLinejoin="round"
                             />
                         </svg>
-                        <span>3</span>
+                        <span>2</span>
                     </Link>
                 </div>
             </div>
